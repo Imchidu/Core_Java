3d7e292edf2bcab45b604535652db6c023d7fdb8
unknown <chidanandxworkz@gmail.com>
2022-09-11 06:32:52 +0530

    a

---
+++ Fridge.java
@@ -0,0 +1,21 @@
+class Fridge{
+	
+  static int weight=89;
+  static double depth=65.5D;
+  static int height=180;
+  static double width=60.9D;
+  static String defrost="auto";
+  static String configuration="two";
+  static String colour="red";
+  static int freezingcapacity=11;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
