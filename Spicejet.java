7b527ff70697c29a2f617b74eeff0be0893ca64f
unknown <chidanandxworkz@gmail.com>
2022-09-19 14:24:09 +0530

    programms

---
+++ Spicejet.java
@@ -0,0 +1,19 @@
+class Spicejet{
+
+  static void flightnos(String[] flightNos)
+     {
+	  System.out.println("Entered flightnos");
+	  System.out.println("no of flights : "+flightNos.length);
+	  
+	  for(int index=0; index<flightNos.length; index++)
+	  {
+		System.out.println("flights nos are : "+flightNos[index]);  
+		  
+	  }
+	  
+	 	  
+	  
+  }
+
+
+}
---
+++ SpicejetRunner.java
@@ -0,0 +1,17 @@
+class SpicejetRunner{
+ 
+ public static void main(String[] args)
+ {
+ System.out.println("start execution");
+
+ String[] flightnos={"Al1825","Al 187","Al 1574","Al 1614","Al 380","Al 1241","Al 1376","Al 131","Al 0127","Al 173"};
+
+    Spicejet.flightnos(flightnos);
+	
+	System.out.println("End execution");
+    
+	
+ }
+
+
+}
