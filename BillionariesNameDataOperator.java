b21cac047d4b0c82899e374df361fd07fb182d18
unknown <chidanandxworkz@gmail.com>
2022-09-28 11:03:20 +0530

    crud

---
+++ BillionariesNameDataOperator.java
@@ -0,0 +1,150 @@
+class BillionariesNameDataOperator
+{
+	static int position=0;
+	static String[] billionariesName={null,null,null,null,null,null,null,null,null,null};
+	
+	static void save(String name)
+	{
+		
+		
+		
+		if(name!=null && name.length()>3)
+		{
+			if(position>=10)
+			{
+				System.err.println("Billionaries Name exceeded");
+				
+				return;
+
+			}
+		
+			
+			System.out.println("Entered Save");
+			billionariesName[position]=name;
+			
+			System.out.println("Billionaries Name is"+billionariesName[position]+"at the position: "+position);
+			position++;
+			System.out.println("End Save");
+			
+		}
+		else
+		{
+			
+			System.err.println("Data not valid "+name);
+			
+		}
+	}
+	static void   displayDetails()
+	{
+		
+		System.out.println("Displaying details");
+		for(int index=0;index<billionariesName.length;index++)
+		{
+			System.out.println("Billionaries Name is : ["+index+"] "+billionariesName[index]);
+		}
+		
+	}
+	
+	static boolean findBillionaries(String billionaries)
+	{
+		
+		for(int index=0;index<billionariesName.length;index++)
+		{
+			if(billionariesName[index]==billionaries)
+			return true;
+		}
+		return false;
+	}
+	
+	static String update(String oldName, String newName)
+	{
+		if(oldName!=null && newName!=null)
+		{
+			System.out.println("Entered update using old name");
+			for(int index=0;index<billionariesName.length;index++)
+			{
+				if(oldName==billionariesName[index])
+				{
+					String ref=billionariesName[index];
+					System.out.println("Billionarie Found: "+ref+" at index: "+index+" updated: "+newName);
+					billionariesName[index]=newName;
+				}
+				
+			}
+		}
+		else{
+			System.out.println("Data invalid");
+			
+			
+		}
+		return null;
+	}
+	
+	
+	static String update(int index, String newName)
+	{
+		if(newName!=null)
+		{
+			System.out.println("Entered update using index");
+			String ref=billionariesName[index];
+			System.out.println("Billionarie Found: "+ref+" updated: "+newName+" at index: "+index);
+			billionariesName[index]=newName;
+			return newName;
+		}
+		
+		else
+		{
+			System.out.println("Data invalid");
+			
+			
+		}
+		return null;
+	}
+	
+	static boolean delete(String name)
+	{
+		if(name!=null)
+		{
+			System.out.println("Entered delete using name");
+			for(int index=0;index<billionariesName.length;index++)
+			{
+				if(name==billionariesName[index])
+				{
+					String ref=billionariesName[index];
+					System.out.println("Billionarie Found: "+ref+" Deleting into null at index: "+index);
+					billionariesName[index]=null;
+					return true;
+				}
+			
+			}
+		}
+		else{
+			System.out.println("Data invalid");
+			
+			
+		}
+		return false;
+	}
+	static boolean delete(int index)
+	{
+		if(billionariesName[index]!=null)
+		{
+			System.out.println("Entered delete using index");
+			billionariesName[index]=null;
+			System.out.println("Deleting into null at index: "+index);
+			return true;
+		}
+		
+		else
+		{
+			System.out.println("Cannot Delete null");
+			
+			
+		}
+		return false;
+	}
+	
+	
+	
+}
+
---
+++ BillionariesNameDataOperatorRunner.java
@@ -0,0 +1,41 @@
+class BillionariesNameDataOperatorRunner{
+
+public static void main(String[] names)
+{
+	System.out.println("Main start");
+	
+	BillionariesNameDataOperator.save("Elon Musk");
+	BillionariesNameDataOperator.save("Bernard Arnault");
+	BillionariesNameDataOperator.save("Gautam Adani");
+	BillionariesNameDataOperator.save("Jeff Bezos");
+	BillionariesNameDataOperator.save("Bill Gates");
+	BillionariesNameDataOperator.save("Warren Buffett");
+	BillionariesNameDataOperator.save("Larry Ellison");
+	BillionariesNameDataOperator.save("Larry Page"); 
+	BillionariesNameDataOperator.save("Mukesh Ambani");
+	BillionariesNameDataOperator.save("Sergey Brin");	
+	
+	BillionariesNameDataOperator.displayDetails();
+	
+	boolean ref=BillionariesNameDataOperator.findBillionaries("Mukesh Ambani");
+	System.out.println("Billionarie found: "+ref);
+	
+	BillionariesNameDataOperator.update("Sergey Brin","Steve Ballmer");
+	BillionariesNameDataOperator.displayDetails();
+	
+	BillionariesNameDataOperator.update(5,"Michael Bloomberg");
+	BillionariesNameDataOperator.displayDetails();
+	
+	boolean del1=BillionariesNameDataOperator.delete("Gautam Adani");
+	System.out.println("Deleted: "+del1);
+	BillionariesNameDataOperator.displayDetails();
+	
+	boolean del2=BillionariesNameDataOperator.delete(6);
+	System.out.println("Deleted: "+del2);
+	BillionariesNameDataOperator.displayDetails();
+	
+	System.out.println("main end");
+}
+
+
+}
